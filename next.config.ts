4522083d304b77878164e9a1e4bc9aa245048334
Luis Paz Cordova <lpaz0073@gmail.com>
2025-05-01 12:49:48 -0500

    feat: add redirect from root to /save in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+	/* config options here */
+	async redirects() {
+		return [
+			{
+				source: '/',
+				destination: '/save',
+				permanent: true,
+			},
+		]
+	},
+}
 
-export default nextConfig;
+export default nextConfig
