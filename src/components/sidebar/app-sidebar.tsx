24cd4b48aa6a966d94c5e35f56854dbb0958d49a
Luis Paz Cordova <lpaz0073@gmail.com>
2025-05-01 12:53:28 -0500

    feat: implement sidebar component with navigation for main tools, projects, and user profile

---
+++ src/components/sidebar/app-sidebar.tsx
@@ -0,0 +1,113 @@
+'use client'
+
+import * as React from 'react'
+import {
+	AudioWaveform,
+	BookOpen,
+	Bot,
+	Command,
+	Frame,
+	GalleryVerticalEnd,
+	Map,
+	PieChart,
+	Settings2,
+	SquareTerminal,
+} from 'lucide-react'
+
+import { NavMain } from '@/components/sidebar/nav-main'
+import { NavProjects } from '@/components/sidebar/nav-projects'
+import { NavUser } from '@/components/sidebar/nav-user'
+import {
+	Sidebar,
+	SidebarContent,
+	SidebarFooter,
+	SidebarHeader,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+	SidebarRail,
+} from '@/components/ui/sidebar'
+import Link from 'next/link'
+
+// This is sample data.
+const data = {
+	user: {
+		name: 'Luis Paz',
+		email: 'lpaz0073@gmail.com',
+		avatar: '/avatars/shadcn.jpg',
+	},
+	navMain: [
+		{
+			title: 'Tools',
+			url: '#',
+			icon: Settings2,
+			isActive: true,
+			items: [
+				{
+					title: 'Save',
+					url: '/save',
+				},
+				{
+					title: 'Chat',
+					url: '/chat',
+				},
+				{
+					title: 'Database',
+					url: '/database',
+				},
+				{
+					title: 'History',
+					url: '/history',
+				},
+			],
+		},
+	],
+	projects: [
+		{
+			name: 'Design Engineering',
+			url: '#',
+			icon: Frame,
+		},
+		{
+			name: 'Sales & Marketing',
+			url: '#',
+			icon: PieChart,
+		},
+		{
+			name: 'Travel',
+			url: '#',
+			icon: Map,
+		},
+	],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+	return (
+		<Sidebar collapsible='icon' {...props}>
+			<SidebarHeader>
+				<SidebarMenu>
+					<SidebarMenuItem>
+						<SidebarMenuButton size='lg' asChild>
+							<Link href='/save'>
+								<div className='flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground'>
+									<GalleryVerticalEnd className='size-4' />
+								</div>
+								<div className='flex flex-col gap-0.5 leading-none'>
+									<span className='font-semibold'>Keepwise</span>
+								</div>
+							</Link>
+						</SidebarMenuButton>
+					</SidebarMenuItem>
+				</SidebarMenu>
+			</SidebarHeader>
+			<SidebarContent>
+				<NavMain items={data.navMain} />
+				<NavProjects projects={data.projects} />
+			</SidebarContent>
+			<SidebarFooter>
+				<NavUser user={data.user} />
+			</SidebarFooter>
+			<SidebarRail />
+		</Sidebar>
+	)
+}
