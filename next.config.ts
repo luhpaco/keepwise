efcbe7ee790c692fb87c7461b8f91d9a6530b859
Luis Paz Cordova <lpaz0073@gmail.com>
2025-05-08 22:39:19 -0500

    refactor: simplify next.config.ts by removing redirects configuration

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,5 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {
-	/* config options here */
-	async redirects() {
-		return [
-			{
-				source: '/',
-				destination: '/save',
-				permanent: true,
-			},
-		]
-	},
-}
+const nextConfig: NextConfig = {}
 
 export default nextConfig
