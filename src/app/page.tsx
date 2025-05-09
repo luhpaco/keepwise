e62ba5489a9a0a24463de00b730b483b77a6bb0a
Luis Paz Cordova <lpaz0073@gmail.com>
2025-05-08 23:00:11 -0500

    feat: create landing page for the app with navigation to /save

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import Link from 'next/link'
+
 export default function Home() {
-	return null
+	// TODO: Create a landing page for the app that redirects to /save
+	return (
+		<div className='flex flex-col items-center justify-center h-screen gap-10'>
+			<h1 className='text-4xl font-bold'>Home Page</h1>
+			<p className='text-lg'>Welcome to the home page</p>
+			<div className='flex gap-4'>
+				<Link href='/save'>Go to app</Link>
+			</div>
+		</div>
+	)
 }
